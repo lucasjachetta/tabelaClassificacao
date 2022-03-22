9babc35bba40a6768f25b568b478a48577763f22
lucasjachetta <lucas.jachetta@gmail.com>
2022-03-22 18:24:06 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,62 @@
+let rafa = { nome: "Rafa", vitorias: 2, empates: 1, derrotas: 1, pontos: 0 };
+let paulo = { nome: "Paulo", vitorias: 1, empates: 1, derrotas: 2, pontos: 0 };
+let gui = { nome: "Gui", vitorias: 1, empates: 1, derrotas: 2, pontos: 0 };
+
+function calculaPontos(jogador) {
+  let pontos = jogador.vitorias * 3 + jogador.empates;
+  return pontos;
+}
+
+rafa.pontos = calculaPontos(rafa);
+paulo.pontos = calculaPontos(paulo);
+gui.pontos = calculaPontos(gui);
+
+let jogadores = [rafa, paulo, gui];
+
+function exibeJogadoresNaTela(jogadores) {
+  let elemento = "";
+
+  for (let i = 0; i < jogadores.length; i++) {
+    elemento += "<tr><td>" + jogadores[i].nome + "</td>";
+    elemento += "<td>" + jogadores[i].vitorias + "</td>";
+    elemento += "<td>" + jogadores[i].empates + "</td>";
+    elemento += "<td>" + jogadores[i].derrotas + "</td>";
+    elemento += "<td>" + jogadores[i].pontos + "</td>";
+    elemento +=
+      "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>";
+    elemento += "</tr>";
+  }
+
+  let tabelaJogadores = document.getElementById("tabelaJogadores");
+  tabelaJogadores.innerHTML = elemento;
+}
+
+exibeJogadoresNaTela(jogadores);
+
+function adicionarVitoria(i) {
+  let jogador = jogadores[i];
+  jogador.vitorias++;
+  jogador.pontos = calculaPontos(jogador);
+  exibeJogadoresNaTela(jogadores);
+}
+
+function adicionarEmpate(i) {
+  let jogador = jogadores[i];
+  jogador.empates++;
+  jogador.pontos = calculaPontos(jogador);
+  exibeJogadoresNaTela(jogadores);
+}
+
+function adicionarDerrota(i) {
+  let jogador = jogadores[i];
+  jogador.derrotas++;
+  jogador.pontos = calculaPontos(jogador);
+  exibeJogadoresNaTela(jogadores);
+}
+
+//Adicionar novo jogador
+//botao de zerar pontos
